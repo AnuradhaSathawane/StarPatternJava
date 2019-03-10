ea27ad9ac2e3322534f142825f7dc3a206117fec
anuradhasathawane <anuradhasath@gmail.com>
2019-03-10 22:25:02 +0000

    pattern

---
+++ src/love/programming/StarPrint3.java
@@ -0,0 +1,18 @@
+package love.programming;
+
+public class StarPrint3 {
+    public static void main(String[] args) {
+
+
+        for(int i=0;i<5;i++){
+            for(int j=0;j<(5);j++){
+                if(j<4-i)
+                    System.out.print(" ");
+                    else
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
